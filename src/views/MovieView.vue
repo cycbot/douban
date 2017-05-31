17d55dcec8f9893191039ae69b5c70d144eded0c
cycbote <cycbot@qq.com>
2017-05-31 14:30:36 +0800

    add movie data to store

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -3,13 +3,15 @@
     <scroller title="影院热映" type="hasCover" :items="hotMovies"></scroller>
     <scroller title="免费在线观影" type="hasCover" :items="topMovies"></scroller>
     <scroller title="新片速递" type="hasCover" :items="newMovies"></scroller>
-    <scroller title="发现好电影" type="onlyString" :items="tags"></scroller>
+    <scroller title="发现好电影" type="onlyString" :items="movieTags"></scroller>
     <types></types>
     <download-app></download-app>
   </div>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+
   import Scroller from '../components/Scroller.vue'
   import DownloadApp from '../components/DownloadApp.vue'
   import Types from '../components/Types.vue'
@@ -18,31 +20,24 @@
     components: { Scroller, Types, DownloadApp},
     data () {
       return {
-        hotMovies: [],
-        newMovies: [],
-        topMovies: []
+
       }
     },
     computed: {
-      tags: function () {
-        return this.$store.state.movieTags
+      ...mapState([
+        'movieTags',
+        'hotMovies',
+        'newMovies',
+        'topMovies'
+      ])
+    },
+    methods: {
+      getMovie: function () {
+        this.$store.dispatch('getMovie')
       }
     },
-    beforeMount () {
-      this.$http.jsonp('http://api.douban.com/v2/movie/in_theaters?count=8')
-        .then(res => {
-          this.hotMovies = res.body.subjects
-        })
-      this.$http.jsonp('http://api.douban.com/v2/movie/coming_soon?count=8')
-        .then(res => {
-          this.newMovies = res.body.subjects
-          console.log(this.newMovies)
-        })
-      this.$http.jsonp('http://api.douban.com/v2/movie/top250?count=8')
-        .then(res => {
-          this.topMovies = res.body.subjects
-          console.log(this.topMovies)
-        })
+    created () {
+        this.getMovie()
     }
   };
 </script>
